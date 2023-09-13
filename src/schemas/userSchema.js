7d0b806991dce6fbb02e44309b6faa5a188976a5
Amandalfs <amandalfs57@gmail.com>
2023-09-13 16:27:07 -0300

    fix: adicionar messagens no schema

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,11 +1,24 @@
 const joi = require("joi");
 
 const userSchema = joi.object().shape({
-    username: joi.string().min(4).max(48).required(),
-    email: joi.string().min(4).max(48).required(),
-    password: joi.string().email({inDomainSegments: 2, tlds: { allow: ["com"] }}).required(),
-    cpf: joi.string().min(11).max(11), 
-    phone: joi.string().max(11),
+    username: joi.string().min(3).required().messages({
+		"any.required": "O campo nome é obrigatório",
+		"string.empty": "O campo nome é obrigatório",
+        "string.min": "O campo nome precisa ter no mínimo 3 caracteres.",
+	}),
+    email: joi.string().email({inDomainSegments: 2, tlds: { allow: ["com"] }}).required().messages({
+        'string.email': 'O campo email precisa ter um formato válido',
+		'any.required': 'O campo email é obrigatório',
+		'string.empty': 'O campo email é obrigatório',
+    }),
+    password: joi.string().min(8).max(32).required().messages({
+        "any.required": "O campo senha é obrigatório",
+		"string.empty": "O campo senha é obrigatório",
+		"string.min": "A senha precisa conter, no mínimo, 5 caracteres",
+        "string.min": "A senha precisa conter, no máximo, 5 caracteres",
+    }),
+    cpf: joi.string().pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/).message("O CPF deve estar no formato XXX.XXX.XXX-XX"), 
+    phone: joi.string().pattern(/^\(\d{2}\) \d{4,5}-\d{4}$/).message("O número de telefone com DDD deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX"),
 });
 
 module.exports = {
