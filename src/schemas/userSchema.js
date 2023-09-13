36244b3f5f5a4f83bbc18f7d2256a30acceae125
Amandalfs <amandalfs57@gmail.com>
2023-09-13 18:48:02 -0300

    feat: criar schema de edit users

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -35,7 +35,27 @@ const sessionsUserSchema = joi.object({
 	})
 });
 
+const editUsersSchema = joi.object({
+	name: joi.string().min(3).required().messages({
+		"any.required": "O campo nome é obrigatório",
+		"string.empty": "O campo nome é obrigatório",
+		"string.min": "O campo nome precisa ter no mínimo 3 caracteres.",
+	}),
+	email: joi.string().email({tlds: { allow: ["com"] }}).required().messages({
+		"string.email": "O campo email precisa ter um formato válido",
+		"any.required": "O campo email é obrigatório",
+		"string.empty": "O campo email é obrigatório",
+	}),
+	newPassword: joi.string().min(8).max(32).messages({
+		"string.min": "A senha precisa conter, no mínimo, 5 caracteres",
+		"string.max": "A senha precisa conter, no máximo, 32 caracteres",
+	}),
+	cpf: joi.string().pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/).message("O CPF deve estar no formato XXX.XXX.XXX-XX"), 
+	phone: joi.string().pattern(/^\(\d{2}\) \d{4,5}-\d{4}$/).message("O número de telefone com DDD deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX"),
+});
+
 module.exports = {
 	userSchema,
 	sessionsUserSchema,
+	editUsersSchema,
 };
