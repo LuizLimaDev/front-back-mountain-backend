b9e6bd6b2c0c27a063972e176b8668cd6eb55f56
Amandalfs <amandalfs57@gmail.com>
2023-09-13 18:48:51 -0300

    feat: adicionar o edit users nas rotas de users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const usersRoutes = Router();
 const { validateRequestBody } = require("../middlewares/validatorCreateUser");
-const { userSchema, sessionsUserSchema } = require("../schemas/userSchema");
+const { userSchema, sessionsUserSchema, editUsersSchema } = require("../schemas/userSchema");
 
-const { createUsers, sessionsUsers, profileUsers } = require("../controllers/usersController");
+const { createUsers, sessionsUsers, profileUsers, editUsers } = require("../controllers/usersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 usersRoutes.post("/", validateRequestBody(userSchema), createUsers);
-usersRoutes.put("/", ensureAuthenticated);
+usersRoutes.put("/", ensureAuthenticated, validateRequestBody(editUsersSchema), editUsers);
 usersRoutes.post("/sessions", validateRequestBody(sessionsUserSchema), sessionsUsers);
 usersRoutes.get("/profile", ensureAuthenticated, profileUsers);
 
