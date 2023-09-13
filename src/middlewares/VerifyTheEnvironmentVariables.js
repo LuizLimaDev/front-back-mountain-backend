d9165f3b5c7afc1b0f7da769b6cf2f6e4ba75c42
Amandalfs <amandalfs57@gmail.com>
2023-09-13 18:55:20 -0300

    feat: criar validacao de variaveis de ambiente

---
+++ src/middlewares/VerifyTheEnvironmentVariables.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const verifyTheEnvironmentVariables = joiSchema => async (req, res, next) => {
+	try {
+		await joiSchema.validateAsync({
+			port: process.env.PORT ?? 3000,
+			secret: process.env.JWT_SECRET, 
+			expiresIn: process.env.EXPIRES_IN,
+		});
+		next();
+	} catch (error) {
+		return res.status(400).json({ mensagem: error.message });
+	}
+};
+
+module.exports = {
+	verifyTheEnvironmentVariables,
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const routes = express();
 const apiHealth = require("../controllers/apiHealth");
 const { usersRoutes } = require("./users.routes");
+const { verifyTheEnvironmentVariables } = require("../middlewares/VerifyTheEnvironmentVariables");
+
+routes.get("/", verifyTheEnvironmentVariables);
 
 routes.get("/", apiHealth);
 routes.use("/users", usersRoutes);
