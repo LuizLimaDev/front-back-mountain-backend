b732852f30508a81abbebdedaccff088c667de8e
Amandalfs <amandalfs57@gmail.com>
2025-01-14 20:29:28 -0300

    feat: criado o teste para verificar se é possivel cadastrar um usuário

--- src/routes/users.e2e.js
+++ src/routes/users.e2e.js
@@ -19,9 +19,20 @@ describe("controller users E2E", () => {
 	});
 
 
-	it("should be possible to create a user", async ()=> {
+	it("should be possible to create a user", async () => {
 		const response = await request(app).post("/users").send(userData); 
 		expect(response.status).toBe(200);
+		expect(response.body.message).toBe("Usuário criado com sucesso");
+	});
+
+	it("should return an error if a user with the same email already exists", async () => {
+		await request(app).post("/users").send(userData); 
+		const response = await request(app).post("/users").send({
+			name: "test 2",
+			email: "test@email.com",
+			password: "12345678"
+		});
+		expect(response.status).toBe(400);
 		expect(response.body.message).toBe("Usuário criado com sucesso");
 	});
 });
